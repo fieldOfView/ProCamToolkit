e84b056bed6cf2c0267f5022d920d6bbe8c679ba
fieldOfView <aldo@fieldofview.com>
2018-01-04 15:58:14 +0100

    Move files into addon structure

---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxMapamok
+	ADDON_DESCRIPTION = Addon version of Mapamok from the YCAMInterlab ProCamToolkit
+	ADDON_AUTHOR = Aldo Hoeben, Kyle McDonald
+	ADDON_TAGS = "3D" "calibration" "mapamok" "ProCam"
+	ADDON_URL = http://github.com/fieldofview/ofxMapamok
+
+common:
+	ADDON_DEPENDENCIES = ofxOpenCv
